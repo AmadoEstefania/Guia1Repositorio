a650ba8488fd260d5b38f81b2cd559555d627502
RodriLang <Rodrigolang90@gmail.com>
2023-08-07 20:29:05 -0300

    constructor Precio

--- src/guia1repositorio/Producto.java
+++ src/guia1repositorio/Producto.java
@@ -3,5 +3,16 @@ package guia1repositorio;
 
 
 public class Producto {
+private int precio;
+    public Producto() {
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
     
 }
