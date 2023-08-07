56e7f6dede6b9a2b2d791713952047e9baa4c02c
RodriLang <Rodrigolang90@gmail.com>
2023-08-07 20:54:37 -0300

    reparacion main

--- src/guia1repositorio/Cristian.java
+++ src/guia1repositorio/Cristian.java
@@ -14,8 +14,5 @@ public class Cristian {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
